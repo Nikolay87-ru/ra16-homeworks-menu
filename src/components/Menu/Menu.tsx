5d530c02d249149581964cb3e4f6dce2a3ff5124
Neto test <nikolajeliseev834@gmail.com>
2025-05-16 12:21:11 +0300

    Доработаны оставшиеся компоненты, стилизация разделена по разным файлам

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,33 +1,35 @@
 import { NavLink } from 'react-router-dom';
-import './Menu.css';
+import './menu.css';
 
-export const Menu = () => {
+const Menu = () => {
   return (
     <nav className="menu">
-      <NavLink 
-        to="/" 
+      <NavLink
+        to="/"
         className={({ isActive }) => `menu__item ${isActive ? 'menu__item-active' : ''}`}
       >
         Гоночное такси
       </NavLink>
-      <NavLink 
-        to="/drift" 
+      <NavLink
+        to="/drift"
         className={({ isActive }) => `menu__item ${isActive ? 'menu__item-active' : ''}`}
       >
         Дрифт-такси
       </NavLink>
-      <NavLink 
-        to="/timeattack" 
+      <NavLink
+        to="/timeattack"
         className={({ isActive }) => `menu__item ${isActive ? 'menu__item-active' : ''}`}
       >
-        Гонка ​​​​​​Time Attack
+        Гонка Time Attack
       </NavLink>
-      <NavLink 
-        to="/forza" 
+      <NavLink
+        to="/forza"
         className={({ isActive }) => `menu__item ${isActive ? 'menu__item-active' : ''}`}
       >
         Forza Karting Sochi
       </NavLink>
     </nav>
   );
-}
+};
+
+export default Menu;
